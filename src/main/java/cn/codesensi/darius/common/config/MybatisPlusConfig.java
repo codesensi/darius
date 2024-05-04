5a94135170a45d04e20cf1a83095cf3f742267ee
codesensi <codesensi@aliyun.com>
2024-05-04 23:07:48 +0800

    feat: 配置Mybatis-Plus防全表更新与删除插件

--- src/main/java/cn/codesensi/darius/common/config/MybatisPlusConfig.java
+++ src/main/java/cn/codesensi/darius/common/config/MybatisPlusConfig.java
@@ -1,6 +1,7 @@
 package cn.codesensi.darius.common.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +15,15 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisPlusConfig {
 
     /**
-     * 乐观锁插件
+     * 插件配置
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // 乐观锁插件
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 防全表更新与删除插件
+        mybatisPlusInterceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
         return mybatisPlusInterceptor;
     }
 }
