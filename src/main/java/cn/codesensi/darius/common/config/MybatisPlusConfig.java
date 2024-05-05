7b78414f7c2e2c9fc0ff43812fef0bf70e3dfe0a
codesensi <codesensi@aliyun.com>
2024-05-05 16:09:38 +0800

    feat: 配置Mybatis-Plus分页插件

--- src/main/java/cn/codesensi/darius/common/config/MybatisPlusConfig.java
+++ src/main/java/cn/codesensi/darius/common/config/MybatisPlusConfig.java
@@ -3,7 +3,7 @@ package cn.codesensi.darius.common.config;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
  * MybatisPlus配置
  */
 @Configuration
-@MapperScan("cn.codesensi.darius.**.mapper")
 public class MybatisPlusConfig {
 
     /**
@@ -20,6 +19,8 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         // 乐观锁插件
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         // 防全表更新与删除插件
