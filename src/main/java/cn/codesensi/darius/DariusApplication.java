dc4146830d3f61dca3b46e64b344b7da151ec5e8
codesensi <codesensi@aliyun.com>
2024-05-05 16:10:17 +0800

    perf: MapperScan配置优化

--- src/main/java/cn/codesensi/darius/DariusApplication.java
+++ src/main/java/cn/codesensi/darius/DariusApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("cn.codesensi.darius.**.mapper")
+@MapperScan("cn.codesensi.darius.*.mapper")
 @SpringBootApplication
 public class DariusApplication {
 
