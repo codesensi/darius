786052b6433ed5e4808cd59324176dd8afd81358
codesensi <codesensi@aliyun.com>
2024-07-27 14:38:45 +0800

    refactor: Caffeine枚举去除多余项

--- src/main/java/cn/codesensi/darius/common/cache/caffeine/CaffeineEnum.java
+++ src/main/java/cn/codesensi/darius/common/cache/caffeine/CaffeineEnum.java
@@ -11,28 +11,23 @@ public enum CaffeineEnum {
     /**
      * 验证码缓存
      */
-    CAPTCHA_IMAGE(CaffeineConstant.CACHE_CAPTCHA, CaffeineConstant.SIZE_DEFAULT, CaffeineConstant.EXPIRE_5_MIN),
+    CAPTCHA_IMAGE(CaffeineConstant.CACHE_CAPTCHA, CaffeineConstant.EXPIRE_5_MIN),
     /**
      * 用户缓存
      */
-    USER_ROLE(CaffeineConstant.CACHE_USER, CaffeineConstant.SIZE_DEFAULT, CaffeineConstant.EXPIRE_DEFAULT);
+    USER_ROLE(CaffeineConstant.CACHE_USER, CaffeineConstant.EXPIRE_DEFAULT),;
 
     /**
      * 缓存名称
      */
     private final String name;
     /**
-     * 最大缓存数量
-     */
-    private final Long maximumSize;
-    /**
      * 缓存过期秒数
      */
     private final Long expire;
 
-    CaffeineEnum(String name, Long maximumSize, Long expire) {
+    CaffeineEnum(String name, Long expire) {
         this.name = name;
-        this.maximumSize = maximumSize;
         this.expire = expire;
     }
 }
