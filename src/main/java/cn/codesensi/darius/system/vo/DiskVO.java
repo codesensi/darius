ee902aee83de7fb34b4ff2fb414e6b26f72345e4
codesensi <codesensi@aliyun.com>
2024-07-28 22:46:07 +0800

    feat: 获取系统信息初版

---
+++ src/main/java/cn/codesensi/darius/system/vo/DiskVO.java
@@ -0,0 +1,12 @@
+package cn.codesensi.darius.system.vo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 磁盘信息
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class DiskVO extends BaseVO {
+}
---
+++ src/main/java/cn/codesensi/darius/system/vo/SwapVO.java
@@ -0,0 +1,12 @@
+package cn.codesensi.darius.system.vo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 交换区信息
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SwapVO extends BaseVO {
+}
